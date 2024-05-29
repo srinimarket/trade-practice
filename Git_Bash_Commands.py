4a021f25258b60b76b3f2d7223e93f730141ceae
srinimarket <srinifolder.py@gmail.com>
2024-05-29 15:28:40 +0530

    Add files via upload

---
+++ Git_Bash_Commands.py
@@ -0,0 +1,19 @@
+# commands to push repositories / files / folders to GitHub
+
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin <GitHub link>
+git push -u origin main
+
+git status
+git add "file name"
+git restore "folder name"
+
+
+git checkout -b my-new-branch
+git add .
+commit -m "New branch"
+push -u origin my-new-branch
+
